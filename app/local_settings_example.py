f3cd22e935034daa6c32dba7b16c4ec361b9e0d1
Rubén Ramírez Gómez <maths.unam@gmail.com>
2022-11-11 00:31:23 -0600

    Initial Commit

---
+++ app/local_settings_example.py
@@ -0,0 +1,37 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "SECRET_KEY"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'webapp.domain.mx'
+]
+
+EMAIL_HOST: 'mail.domain.com.mx'
+EMAIL_PORT: '465'
+EMAIL_HOST_USER: 'correo@domain.com.mx'
+EMAIL_HOST_PASSWORD: 'MAIL_PWD'
+EMAIL_USE_SSL: True
+DEFAULT_FROM_EMAIL: 'Sistemas IMGX<no-reply@domain.com.mx>'
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '10.5.0.5',
+        'PORT': '3306',
+        'OPTIONS': {
+            'read_default_file': 'app/db_data.imgx',
+            'init_command': 'SET default_storage_engine=INNODB',
+            'autocommit': True,
+        },
+        'NAME': 'DB_NAME',
+        'USER': 'USER',
+        'PASSWORD': 'PWD',
+    }
+}
